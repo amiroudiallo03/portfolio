cd76e125a262ab925a687a046117eb3a3c57cb39
amiroudiallo03 <amiroudiallo03@gmail.com>
2022-07-04 14:55:02 +0000

    portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "d3j6i3o1bojl0j",
-        "USER": "ltigoupkhgtpuf",
-        "PASSWORD": "82d49c3d490680937460bb0bf04871acb58be299f13c1e56aa9ff3953249f181",
-        "HOST": "ec2-52-49-68-244.eu-west-1.compute.amazonaws.com",
-        "PORT": "5432",
-
-    }
-}
-
-
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "d3j6i3o1bojl0j",
+#         "USER": "ltigoupkhgtpuf",
+#         "PASSWORD": "82d49c3d490680937460bb0bf04871acb58be299f13c1e56aa9ff3953249f181",
+#         "HOST": "ec2-52-49-68-244.eu-west-1.compute.amazonaws.com",
+#         "PORT": "5432",
+
 #     }
 # }
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
