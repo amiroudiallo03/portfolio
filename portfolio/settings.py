fba8bd932eeb503dfe8634405541889aef6d897d
amiroudiallo03 <amiroudiallo03@gmail.com>
2022-07-05 11:44:38 +0000

    pythonanywher

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'portfolio.urls'
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
